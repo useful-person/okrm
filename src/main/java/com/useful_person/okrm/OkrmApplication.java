b60e478f085f37b1f7c72fbdc098d9a28ae27851
chenbitao <cbtpro@163.com>
2025-03-18 14:43:00 +0800

    feat: init

---
+++ src/main/java/com/useful_person/okrm/OkrmApplication.java
@@ -0,0 +1,13 @@
+package com.useful_person.okrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrmApplication.class, args);
+	}
+
+}
